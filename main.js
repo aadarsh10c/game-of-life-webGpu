97ff6e6e1f5d3c3adaec165bc3d5e89d98a62c43
Adarsh Kumar <adarsh010c@gmail.com>
2023-05-20 23:20:17 +0530

    Draw Geometry, rendered my first square

--- main.js
+++ main.js
@@ -21,6 +21,76 @@ context.configure({
   format: canvasFormat
 })
 
+
+
+//Create a typed  array to hold vertices of square 
+const vertices = new Float32Array([
+//   X,    Y,
+-0.8, -0.8, // Triangle 1 (Blue)
+0.8, -0.8,
+0.8,  0.8,
+
+-0.8, -0.8, // Triangle 2 (Red)
+0.8,  0.8,
+-0.8,  0.8,
+])
+// console.log( vertices.byteLength)
+//Create a gpu buffer
+const vertexBuffer = device.createBuffer({
+  label: "Cell vertices",
+  size: vertices.byteLength,
+  usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
+});
+
+//Copy vertices tata into the buffer
+device.queue.writeBuffer(vertexBuffer, /*bufferOffset=*/0, vertices);
+
+
+//Define the vertex layout
+const vertexBufferLayout = {
+  arrayStride: 8,
+  attributes: [{
+    format: "float32x2",
+    offset: 0,
+    shaderLocation: 0, // Position, see vertex shader
+  }],
+};
+
+
+//Define vertex Shader
+const cellShaderModule = device.createShaderModule({
+  label: "Cell shader",
+  code: `
+  @vertex
+  fn vertexMain(@location(0) pos: vec2f) ->
+    @builtin(position) vec4f {
+    return vec4f(pos, 0, 1);
+  }
+  @fragment
+  fn fragmentMain() -> @location(0) vec4f {
+    return vec4f( 1, 0, 0, 1);
+  }
+  `
+});
+
+//Create a render pipleline
+const cellPipeline = device.createRenderPipeline({
+  label: "Cell pipeline",
+  layout: "auto",
+  vertex: {
+    module: cellShaderModule,
+    entryPoint: "vertexMain",
+    buffers: [vertexBufferLayout]
+  },
+  fragment: {
+    module: cellShaderModule,
+    entryPoint: "fragmentMain",
+    targets: [{
+      format: canvasFormat
+    }]
+  }
+});
+
 //Configure GPU command encoder
 const encoder = device.createCommandEncoder()
 
@@ -34,11 +104,15 @@ const pass = encoder.beginRenderPass({
   }]
 })
 
+pass.setPipeline(cellPipeline);
+pass.setVertexBuffer(0, vertexBuffer);
+pass.draw(vertices.length / 2); // 6 vertices
+
 pass.end()
 //test
-const commandBuffer = encoder.finish()
 
-device.queue.submit([commandBuffer])
 
 // Finish the command buffer and immediately submit it.
 device.queue.submit([encoder.finish()]);
+
+
